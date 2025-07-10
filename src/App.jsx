2a45efa8fff7a766d5c33c4b803401fa23d5e785
Varun <varunjethani@outlook.com>
2025-07-10 18:12:50 +0530

    add gauth

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,10 @@ import ContactUs from "./components/Contact/ContactUs";
 import Profile from "./components/Profile/Profile";
 import CompanyProfile from "./components/AboutUs/CompanyProfile";
 import CareersPage from "./components/Career/Career";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 const App = () => {
   axios.defaults.baseURL = API_BASE_URL;
@@ -27,11 +29,20 @@ const App = () => {
     }
   }, [dispatch]);
   axios.defaults.withCredentials = true;
+
+  const GoogleAuthwrapper = () => {
+    return(
+      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        <AuthPage/>
+      </GoogleOAuthProvider>
+    )
+  }
+
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/auth" element={<GoogleAuthwrapper />} />
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<OurProducts />} />
         <Route path="/about-us" element={<About />} />
